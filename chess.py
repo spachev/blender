7c22f8b2850c368f740f2e25658ada6f9330c0d3
Sasha Pachev <sasha@asksasha.com>
2017-10-30 21:29:48 -0600

    Finished generating the pawn bottom.

--- chess.py
+++ chess.py
@@ -30,12 +30,15 @@ def get_args():
 	parsed_script_args, _ = parser.parse_known_args(script_args)
 	return parsed_script_args
 
+def base_cur_r(x):
+	return (base_top_r - r)/(base_h * base_h) * x * x + r
+
 args = get_args()
 
 origin = (0,0,0)
 o_v = Vector(origin)
 
-rq = 0.5 # ratio pawn base height to the radius
+rq = 0.5 # ratio pawn base top to bottom radius
 h = float(args.height)
 r = float(args.radius)
 th = float(args.thickness)
@@ -43,28 +46,46 @@ n = int(args.num_parts)
 vn = int(args.num_vparts)
 piece = args.piece
 print("h=" + str(h))
-x = 0
-dx = rq * r / float(vn)
-verts = []
-faces = []
-cur_r = r
 
-for v_ind in range(0,vn):
-	cur_r = cur_r - x
-	cur_verts = mu.get_circle_verts((0,0,cur_r - x), cur_r, n)
-	cur_n_verts = len(verts)
-	verts += cur_verts
-	x += dx
+def mk_file_path(orig_fname, suffix):
+	parts = orig_fname.split(".")
+	if len(parts) < 2:
+		return orig_fname + "-" + suffix + ".stl"
+	return parts[0] + "-" + suffix + "." + parts[1]
 
-	if v_ind > 0:
-		for i in range(0, n):
-			base_low = (v_ind - 1) * n
-			next_i = base_low + (i + 1) % n
-			#print(base_low)
-			faces.append([ base_low + i, next_i, next_i + n,
-						base_low + i + n ])
+def make_pawn_bottom():
+	x = 0.0
+	dx = base_h / float(vn)
+	verts = []
+	faces = []
+	cur_r = r
 
-#print("n_verts="+str(len(verts)))
-#print(faces)
-mu.create_mesh_from_data('Pawn', o_v, verts, faces, True)
-bpy.ops.export_mesh.stl(filepath=args.save,ascii=False)
+	for v_ind in range(0,vn):
+		cur_r = base_cur_r(x)
+		# print("x=" + str(x) + ", cur_r = " + str(cur_r))
+		cur_verts = mu.get_circle_verts((0,0,x), cur_r, n)
+		cur_n_verts = len(verts)
+		verts += cur_verts
+		x += dx
+
+		if v_ind > 0:
+			for i in range(0, n):
+				base_low = (v_ind - 1) * n
+				next_i = base_low + (i + 1) % n
+				#print(base_low)
+				faces.append([ base_low + i, next_i, next_i + n,
+							base_low + i + n ])
+
+	print("x="+str(x) + " dx=" + str(dx))
+	#print("n_verts="+str(len(verts)))
+	#print(faces)
+	faces.append(range(0,n))
+	faces.append(range((vn - 1) * n, vn * n))
+	mu.create_mesh_from_data('Pawn', o_v, verts, faces, True)
+	bpy.ops.export_mesh.stl(filepath=mk_file_path(args.save,"bottom"),ascii=False)
+
+base_top_r = r * rq
+base_q = 0.2
+base_h = h * base_q
+
+make_pawn_bottom()
--- pawn.sh
+++ pawn.sh
@@ -9,5 +9,5 @@ function die
 OUT_FILE=${OUT_FILE:-pawn.stl}
 PAWN_PARTS=3
 
-blender -b -P chess.py -- --save $OUT_FILE --height 50 --thickness 3 --num-parts 70 \
+blender -b -P chess.py -- --save $OUT_FILE --height 500 --thickness 3 --num-parts 70 \
 	--num-vparts 20 --radius 120 --piece pawn
