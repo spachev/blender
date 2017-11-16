d499507a7b63c83d2480973d3a561859233fb49d
Sasha Pachev <sasha@asksasha.com>
2017-11-15 21:34:09 -0700

    Chess pieces: Bishop is complete.

--- chess.py
+++ chess.py
@@ -87,9 +87,17 @@ class Piece3(Piece):
 		self.base_top_r = r * self.rq
 		self.base_h = h * self.base_q()
 		self.middle_h = h * self.middle_q()
+		self.vn_base = int(vn * self.base_q())
+		self.vn_middle = int(vn * self.middle_q())
+		self.vn_top = int(vn * self.top_q())
+		self.top_h = h * self.total_q() - self.base_h - self.middle_h
 
+	# used for stretching piece
+	# in particular pawns should be shorter than other pieces
+	def total_q(self):
+		return 1.0
 	def top_q(self):
-		return 1.0 - self.base_q() - self.middle_q()
+		return self.total_q() - self.base_q() - self.middle_q()
 
 	def make_base(self, all_in_one):
 		self.make_conic_part(0, self.base_h, self.vn_base, self.base_cur_r,
@@ -128,7 +136,6 @@ class Pawn(Piece3):
 		# top_h ^ 2 - 2 * top_h * top_r = - middle_top_r ^ 2
 		# top_r = ( top_h ^ 2  + middle_top_r ^ 2)/ (2*top_h)
 
-		self.top_h = h - self.base_h - self.middle_h
 		self.top_r = ( self.top_h * self.top_h  + \
 			self.middle_top_r * self.middle_top_r) / (2.0 * self.top_h)
 		self.top_shift_h = self.top_h - self.top_r
@@ -136,10 +143,6 @@ class Pawn(Piece3):
 		print("base_h=" + str(self.base_h) + " bend_h = " + str(self.bend_h))
 		print("bend_r=" + str(self.bend_r))
 		print("top_shift_h = " + str(self.top_shift_h) + " top_h = " + str(self.top_h))
-
-		self.vn_base = int(vn * self.base_q())
-		self.vn_middle = int(vn * self.middle_q())
-		self.vn_top = int(vn * self.top_q())
 
 	def get_piece_name(self):
 		return "Pawn"
@@ -178,25 +181,52 @@ class Pawn(Piece3):
 		else:
 			return 0
 
-
-class Bishop(Piece):
+# real bishop: top_max_r = 6
+# bend_r = 4
+class Bishop(Piece3):
 	def __init__(self):
 		super().__init__()
+		self.bend_rq = 0.6 # ratio of bend_top_r to base_top_r
+		self.bend_q = 0.7
+		self.sin_q = 2.0
+		# joint point of sinusoides
+		self.sin_joint_h =  self.top_h *  1.0/(self.sin_q + 1.0)
+		print("sin_joint_h="+str(self.sin_joint_h))
+		self.top_max_rq = 1.5
+		self.bend_r = self.base_top_r * self.bend_rq
+		self.top_max_r = self.bend_r * self.top_max_rq
+		self.bend_h = self.base_h + self.middle_h * self.bend_q
 		self.middle_top_r = self.middle_cur_r(self.middle_h + self.base_h)
-		self.top_h = h - self.base_h - self.middle_h
-		self.top_r = ( self.top_h * self.top_h  + \
-			self.middle_top_r * self.middle_top_r) / (2.0 * self.top_h)
+		self.sin_k = math.pi / (2.0*self.sin_joint_h)
+		self.shift_top_x = math.asin(self.bend_r / self.top_max_r) / self.sin_k
+		self.top_h = h * (self.total_q() - self.base_q() - \
+			self.middle_q()) - self.shift_top_x
 
-		self.vn_base = int(vn * self.base_q())
-		self.vn_middle = int(vn * self.middle_q())
-		self.vn_top = int(vn * self.top_q())
+	def base_cur_r(self, x):
+		return (self.base_top_r - r)/(self.base_h * self.base_h) * x * x + r
+	def middle_cur_r(self, x):
+		if x > self.bend_h:
+			return self.bend_r
+		return (self.base_top_r - self.bend_r) / \
+			((self.base_h - self.bend_h) * \
+			(self.base_h - self.bend_h)) * (x - self.bend_h) \
+		* (x - self.bend_h) + self.bend_r
+	def top_cur_r(self,x):
+		x -= self.base_h + self.middle_h - self.shift_top_x
+		#print("x=" + str(x))
+		if x < self.sin_joint_h:
+			return self.top_max_r * math.sin(self.sin_k * x)
+		return self.top_max_r * math.cos(math.pi*(x - \
+			self.sin_joint_h)/(2.0 * self.sin_joint_h * self.sin_q))
 
 	def get_piece_name(self):
 		return "Bishop"
+	def total_q(self):
+		return 1.2
 	def base_q(self):
-		return 0.25
+		return 0.3
 	def middle_q(self):
-		return 0.4
+		return 0.5
 
 args = get_args()
 
@@ -212,4 +242,6 @@ piece = args.piece
 
 pawn = Pawn()
 pawn.make()
+bishop = Bishop()
+bishop.make()
 
